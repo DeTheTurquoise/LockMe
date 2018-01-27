1ec4ff0b0b862722f5b9cbc35aab2ae1282ad6fd
DMM <dominikamatyjewiczmaciejewicz@gmail.com>
2018-01-27 22:34:23 +0100

    done is better than perfect

--- app/src/main/java/dmm/lockme/Lock.java
+++ app/src/main/java/dmm/lockme/Lock.java
@@ -1,30 +1,34 @@
 package dmm.lockme;
 
+import android.app.admin.DeviceAdminReceiver;
 import android.app.admin.DevicePolicyManager;
-import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class Lock extends AppCompatActivity {
+public class Lock extends AppCompatActivity{
+    protected static final int REQUEST_CODE_ENABLE_ADMIN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lock);
 
-    }
-
-    public class LockReceiver extends BroadcastReceiver{
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-
-            DevicePolicyManager mDPM = (DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE);
-            ComponentName componentName;
+        DevicePolicyManager mDPM = (DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE);
+        ComponentName mDeviceAdminSample = new ComponentName(this, LockReceiver.class);
+        if (!mDPM.isAdminActive(mDeviceAdminSample)){
+            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mDeviceAdminSample);
+            startActivityForResult(intent, REQUEST_CODE_ENABLE_ADMIN);
+        }
+        else {
             mDPM.lockNow();
         }
+        this.finishAffinity();
+    }
+
+    public class LockReceiver extends DeviceAdminReceiver{
+
     }
 }
