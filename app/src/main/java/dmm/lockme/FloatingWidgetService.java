126663e7581882a7a44cf6d52fd30e4b40aef523
DMM <dominikamatyjewiczmaciejewicz@gmail.com>
2018-02-14 13:56:58 +0100

    with widget commented code - WIP

--- app/src/main/java/dmm/lockme/FloatingWidgetService.java
+++ app/src/main/java/dmm/lockme/FloatingWidgetService.java
@@ -5,8 +5,6 @@ import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
-import android.graphics.Point;
-import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.view.Gravity;
@@ -23,8 +21,16 @@ public class FloatingWidgetService extends Service implements View.OnClickListen
 
     private WindowManager windowManager;
     private View floatingWidgetView;
-    private int x_init_cord, y_init_cord, x_init_margin, y_init_margin;
-    private Point szWindow = new Point();
+    private final static int MOVE_MARGIN = 5;
+    private final static int TIME_MARGIN = 300;
+    private WindowManager.LayoutParams params;
+    private int xWhenPressed = 0;
+    private int yWhenPressed = 0;
+    private long timeWhenPressed = 0;
+
+//    public FloatingWidgetService(WindowManager.LayoutParams params) {
+//        this.params = params;
+//    }
 
     @Nullable
     @Override
@@ -41,22 +47,21 @@ public class FloatingWidgetService extends Service implements View.OnClickListen
     public void onCreate() {
         super.onCreate();
 
-        //init WindowManager
+        //initialize WindowManager and LayoutInflater
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-
-        //Init LayoutInflater
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
 
+        //prepare floating widget
         addFloatingWidgetView(inflater);
+
+        //
         floatingWidgetOnTouchListener();
     }
 
     private void addFloatingWidgetView(LayoutInflater inflater) {
-        //Inflate the floating view layout we created
         floatingWidgetView = inflater.inflate(R.layout.lock_widget, null);
 
-        //Add the view to the window.
-        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_PHONE,
@@ -64,7 +69,7 @@ public class FloatingWidgetService extends Service implements View.OnClickListen
                 PixelFormat.TRANSLUCENT);
 
         //Specify the view position
-        params.gravity = Gravity.TOP | Gravity.LEFT;
+        params.gravity = Gravity.TOP | Gravity.END;
 
         //Add the view to the window
         windowManager.addView(floatingWidgetView, params);
@@ -73,121 +78,40 @@ public class FloatingWidgetService extends Service implements View.OnClickListen
 
     private void floatingWidgetOnTouchListener(){
         floatingWidgetView.findViewById(R.id.floating_widget_layout).setOnTouchListener(new View.OnTouchListener() {
-
-            long timeStart = 0;
-            long timeEnd = 0;
-
-            boolean isLongClick = false;//variable to judge if user click long press
-
-            Handler handler_longClick = new Handler();
-            Runnable runnable_longClick = new Runnable() {
-                @Override
-                public void run() {
-                    //On Floating Widget Long Click
-
-                    //Set isLongClick as true
-                    isLongClick = true;
-
-                //    onFloatingWidgetLongClick();
-                }
-            };
-
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-
-                //Get Floating widget view params
-                WindowManager.LayoutParams layoutParams = (WindowManager.LayoutParams) floatingWidgetView.getLayoutParams();
-
-                //get the touch location coordinates
-                int x_cord = (int) event.getRawX();
-                int y_cord = (int) event.getRawY();
-
-                int x_cord_Destination, y_cord_Destination;
-
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        timeStart = System.currentTimeMillis();
-
-                        handler_longClick.postDelayed(runnable_longClick, 600);
-
-                        x_init_cord = x_cord;
-                        y_init_cord = y_cord;
-
-                        //remember the initial position.
-                        x_init_margin = layoutParams.x;
-                        y_init_margin = layoutParams.y;
-
-                        return true;
-                    case MotionEvent.ACTION_UP:
-                        isLongClick = false;
-                        handler_longClick.removeCallbacks(runnable_longClick);
-
-                        //Get the difference between initial coordinate and current coordinate
-                        int x_diff = x_cord - x_init_cord;
-                        int y_diff = y_cord - y_init_cord;
-
-                        //The check for x_diff <5 && y_diff< 5 because sometime elements moves a little while clicking.
-                        //So that is click event.
-                        if (Math.abs(x_diff) < 5 && Math.abs(y_diff) < 5) {
-                            timeEnd = System.currentTimeMillis();
-
-                            //Also check the difference between start time and end time should be less than 300ms
-                            if ((timeEnd - timeStart) < 300)
-                                onFloatingWidgetClick();
-
-                        }
-
-                        y_cord_Destination = y_init_margin + y_diff;
-
-//                        int barHeight = getStatusBarHeight();
-                        int barHeight = 20;
-                        if (y_cord_Destination < 0) {
-                            y_cord_Destination = 0;
-                        } else if (y_cord_Destination + (floatingWidgetView.getHeight() + barHeight) > szWindow.y) {
-                            y_cord_Destination = szWindow.y - (floatingWidgetView.getHeight() + barHeight);
-                        }
-
-                        layoutParams.y = y_cord_Destination;
-
-//                        inBounded = false;
-
-                        //reset position if user drags the floating view
-//                        resetPosition(x_cord);
-
-                        return true;
-                    case MotionEvent.ACTION_MOVE:
-                        int x_diff_move = x_cord - x_init_cord;
-                        int y_diff_move = y_cord - y_init_cord;
-
-                        x_cord_Destination = x_init_margin + x_diff_move;
-                        y_cord_Destination = y_init_margin + y_diff_move;
-
-                        //If user long click the floating view, update remove view
-                        if (isLongClick) {
-
-                                //Update the layout with new X & Y coordinate
-                                windowManager.updateViewLayout(floatingWidgetView, layoutParams);
-                                break;
-                            } else {
-                                //If Floating window gets out of the Remove view update Remove view again
-//                                inBounded = false;
-                                onFloatingWidgetClick();
-                            }
-
-
-//                        layoutParams.x = x_cord_Destination;
-//                        layoutParams.y = y_cord_Destination;
-
-                        //Update the layout with new X & Y coordinate
-                        windowManager.updateViewLayout(floatingWidgetView, layoutParams);
-                        return true;
-                }
+//                long timeNow = System.currentTimeMillis();
+//                int xNow = (int) event.getRawX();
+//                int yNow = (int) event.getRawY();
+//
+//                switch (event.getAction()){
+//                    case MotionEvent.ACTION_DOWN:
+//                        xWhenPressed = xNow;
+//                        yWhenPressed = yNow;
+//                        timeWhenPressed = System.currentTimeMillis();
+//                        return true;
+//                    case MotionEvent.ACTION_UP:
+//                        if (xNow > xWhenPressed + MOVE_MARGIN){
+//                            params.gravity = Gravity.START;
+//                        }
+//                        else if(xNow + MOVE_MARGIN < xWhenPressed){
+//                            params.gravity = Gravity.END;
+//                        }
+//
+//                        if (yNow > yWhenPressed + MOVE_MARGIN){
+//                            params.gravity = Gravity.BOTTOM;
+//                        }
+//                        else if(yNow + MOVE_MARGIN < yWhenPressed){
+//                            params.gravity = Gravity.TOP;
+//                        }
+//                        if(timeNow - timeWhenPressed > TIME_MARGIN){
+//                            Lock.lockScreen((DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE));
+//                        }
+//                        return true;
+//                }
+                Lock.lockScreen((DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE));
                 return false;
             }
         });
     }
-    private void onFloatingWidgetClick() {
-        Lock.lockScreen((DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE));
-    }
-
 }
--- app/src/main/java/dmm/lockme/Lock.java
+++ app/src/main/java/dmm/lockme/Lock.java
@@ -5,11 +5,16 @@ import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 
 public class Lock extends AppCompatActivity{
-    protected static final int REQUEST_CODE_ENABLE_ADMIN = 1;
+    private static final int REQUEST_CODE_ENABLE_ADMIN = 1;
+    private static final int DRAW_OVER_OTHER_APP_PERMISSION_REQUEST_CODE = 1222;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +22,31 @@ public class Lock extends AppCompatActivity{
 
         DevicePolicyManager mDPM = (DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE);
         ComponentName mDeviceAdminSample = new ComponentName(this, LockReceiver.class);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            //If the draw over permission is not available open the settings screen
+            //to grant the permission.
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, DRAW_OVER_OTHER_APP_PERMISSION_REQUEST_CODE);
+        }
+        else{
+            startService(new Intent(Lock.this, FloatingWidgetService.class));
+        }
+
         if (!mDPM.isAdminActive(mDeviceAdminSample)){
             Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
             intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mDeviceAdminSample);
             startActivityForResult(intent, REQUEST_CODE_ENABLE_ADMIN);
         }
         else {
-            mDPM.lockNow();
+            lockScreen(mDPM);
         }
         this.finishAffinity();
+    }
+
+    public static void lockScreen(DevicePolicyManager devicePolicyManager){
+        devicePolicyManager.lockNow();
     }
 
     public class LockReceiver extends DeviceAdminReceiver{
