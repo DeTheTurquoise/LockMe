ee198f6882296b620c4e170bc6444259b424e1dd
DMM <dominikamatyjewiczmaciejewicz@gmail.com>
2018-04-26 14:20:38 +0200

    LockMe 1.0 First release

--- app/src/main/java/dmm/lockme/FloatingWidgetService.java
+++ app/src/main/java/dmm/lockme/FloatingWidgetService.java
@@ -21,16 +21,25 @@ public class FloatingWidgetService extends Service implements View.OnClickListen
 
     private WindowManager windowManager;
     private View floatingWidgetView;
-    private final static int MOVE_MARGIN = 5;
-    private final static int TIME_MARGIN = 300;
-    private WindowManager.LayoutParams params;
+    private final static int TIME_MARGIN = 100;
+    private WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.TYPE_PHONE,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+            PixelFormat.TRANSLUCENT);
     private int xWhenPressed = 0;
     private int yWhenPressed = 0;
     private long timeWhenPressed = 0;
-
-//    public FloatingWidgetService(WindowManager.LayoutParams params) {
-//        this.params = params;
+    private boolean isTop = true;
+    private boolean isRight = true;
+//
+//    public boolean isServiceActive() {
+//        return isServiceActive;
 //    }
+//
+//    private boolean isServiceActive = false;
+
 
     @Nullable
     @Override
@@ -54,21 +63,16 @@ public class FloatingWidgetService extends Service implements View.OnClickListen
         //prepare floating widget
         addFloatingWidgetView(inflater);
 
-        //
+        //prepare widget listener
         floatingWidgetOnTouchListener();
     }
 
+
     private void addFloatingWidgetView(LayoutInflater inflater) {
+        //set layout to widget
         floatingWidgetView = inflater.inflate(R.layout.lock_widget, null);
 
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_PHONE,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT);
-
-        //Specify the view position
+        //Specify the view position - right top corner
         params.gravity = Gravity.TOP | Gravity.END;
 
         //Add the view to the window
@@ -77,39 +81,71 @@ public class FloatingWidgetService extends Service implements View.OnClickListen
 
 
     private void floatingWidgetOnTouchListener(){
+        //implement onTouchListener
         floatingWidgetView.findViewById(R.id.floating_widget_layout).setOnTouchListener(new View.OnTouchListener() {
             @Override
+            //returns true when action was performed
             public boolean onTouch(View v, MotionEvent event) {
-//                long timeNow = System.currentTimeMillis();
-//                int xNow = (int) event.getRawX();
-//                int yNow = (int) event.getRawY();
-//
-//                switch (event.getAction()){
-//                    case MotionEvent.ACTION_DOWN:
-//                        xWhenPressed = xNow;
-//                        yWhenPressed = yNow;
-//                        timeWhenPressed = System.currentTimeMillis();
-//                        return true;
-//                    case MotionEvent.ACTION_UP:
-//                        if (xNow > xWhenPressed + MOVE_MARGIN){
-//                            params.gravity = Gravity.START;
-//                        }
-//                        else if(xNow + MOVE_MARGIN < xWhenPressed){
-//                            params.gravity = Gravity.END;
-//                        }
-//
-//                        if (yNow > yWhenPressed + MOVE_MARGIN){
-//                            params.gravity = Gravity.BOTTOM;
-//                        }
-//                        else if(yNow + MOVE_MARGIN < yWhenPressed){
-//                            params.gravity = Gravity.TOP;
-//                        }
-//                        if(timeNow - timeWhenPressed > TIME_MARGIN){
-//                            Lock.lockScreen((DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE));
-//                        }
-//                        return true;
-//                }
-                Lock.lockScreen((DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE));
+
+                switch (event.getAction()){
+                    //ACTION_DOWN - when widget is pressed
+                    case MotionEvent.ACTION_DOWN:
+                        //set values to basic variables - check position and time of click
+                        xWhenPressed = (int) event.getRawX();
+                        yWhenPressed = (int) event.getRawY();
+                        timeWhenPressed = System.currentTimeMillis();
+                        return true;
+                    //ACTION_UP - when pressed gesture has finished
+                    case MotionEvent.ACTION_UP:
+                        //check current time
+                        long timeNow = System.currentTimeMillis();
+                        //calculate gesture position change when widget was pressed
+                        int xDiff = (int) event.getRawX() - xWhenPressed;
+                        int yDiff = (int) event.getRawY() - yWhenPressed;
+                        //check if gesture was a move to top right corner
+                        if(((xDiff > 0)&&(yDiff < 0))||
+                                ((xDiff > 0)&& (yDiff == 0)&& isTop)||
+                                ((xDiff == 0) && (yDiff < 0) && isRight)){
+                            //change values of current widget position
+                            isRight = true;
+                            isTop = true;
+                            //set new position to top right
+                            params.gravity = Gravity.TOP | Gravity.END;
+                        }
+                        //check if gesture was a move to bottom right corner
+                        else if(((xDiff > 0)&&(yDiff > 0))||
+                                ((xDiff > 0)&& (yDiff == 0)&& !isTop)||
+                                ((xDiff == 0) && (yDiff > 0) && isRight)){
+                            isRight = true;
+                            isTop = false;
+                            params.gravity = Gravity.BOTTOM | Gravity.END;
+                        }
+                        //check if gesture was a move to bottom left corner
+                        else if(((xDiff == 0) && (yDiff > 0) && !isRight)||
+                                ((xDiff < 0)&& (yDiff == 0)&& !isTop)||
+                                ((xDiff < 0)&& (yDiff > 0))){
+                            isRight = false;
+                            isTop = false;
+                            params.gravity = Gravity.BOTTOM | Gravity.START;
+                        }
+                        //check if gesture was a move to top left
+                        else if(((xDiff == 0) && (yDiff > 0)&& !isRight)||
+                                ((xDiff < 0)&& (yDiff < 0))||
+                                ((xDiff < 0)&& (yDiff == 0)&& isTop)){
+                            isRight = false;
+                            isTop = true;
+                            params.gravity = Gravity.TOP | Gravity.START;
+                        }
+
+                        //update new widget position if it was changed
+                        windowManager.updateViewLayout(floatingWidgetView,params);
+
+                        //if user just clicked this widget then lock screen
+                        if(timeNow - timeWhenPressed < TIME_MARGIN){
+                            Lock.lockScreen((DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE));
+                        }
+                        return true;
+                }
                 return false;
             }
         });
--- app/src/main/java/dmm/lockme/Lock.java
+++ app/src/main/java/dmm/lockme/Lock.java
@@ -16,40 +16,107 @@ public class Lock extends AppCompatActivity{
     private static final int DRAW_OVER_OTHER_APP_PERMISSION_REQUEST_CODE = 1222;
 
 
+    // when user uses application - lock screen
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        performLock();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        performLock();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        performLock();
+    }
+
+    //activate lock widget and lock screen
+
+    private void performLock(){
+        //create DevicePolicyManager
         DevicePolicyManager mDPM = (DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE);
+        //create component of lock class
         ComponentName mDeviceAdminSample = new ComponentName(this, LockReceiver.class);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
-            //If the draw over permission is not available open the settings screen
-            //to grant the permission.
-            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:" + getPackageName()));
-            startActivityForResult(intent, DRAW_OVER_OTHER_APP_PERMISSION_REQUEST_CODE);
-        }
-        else{
+        //if you have permission to display alerts (and admin permission) - start lock widget service
+        if (permissionToStartService(mDPM,mDeviceAdminSample)){
             startService(new Intent(Lock.this, FloatingWidgetService.class));
         }
 
-        if (!mDPM.isAdminActive(mDeviceAdminSample)){
-            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mDeviceAdminSample);
-            startActivityForResult(intent, REQUEST_CODE_ENABLE_ADMIN);
-        }
-        else {
+        //if you have admin permission - lock screen
+        if(permissionToLock(mDPM,mDeviceAdminSample)){
             lockScreen(mDPM);
         }
-        this.finishAffinity();
+
+        //close task
+        finish();
     }
 
+    //use DevicePolicyManager to lock screen
     public static void lockScreen(DevicePolicyManager devicePolicyManager){
         devicePolicyManager.lockNow();
     }
 
-    public class LockReceiver extends DeviceAdminReceiver{
-
+    //display request for alert permission
+    private void askForDraw(){
+        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:" + getPackageName()));
+        startActivityForResult(intent, DRAW_OVER_OTHER_APP_PERMISSION_REQUEST_CODE);
+        finish();
     }
+
+    //display request for admin permission
+    private void askForAdmin(ComponentName mDeviceAdminSample){
+        Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mDeviceAdminSample);
+        startActivityForResult(intent, REQUEST_CODE_ENABLE_ADMIN);
+        finish();
+    }
+
+    //function checks if app has permission to display alerts
+    private boolean permissionToStartService(DevicePolicyManager mDPM,ComponentName mDeviceAdminSample){
+        //check if app has admin permission
+        //it is important - service will lock screen and needs this permission
+        if(permissionToLock(mDPM,mDeviceAdminSample)){
+            //check if app has permission to draw alerts
+            if((Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this))){
+                //if does not - display request
+                askForDraw();
+            }
+            else {
+                return true;
+            }
+            //if app didn't have permission at first, check if user added permission by request above
+            if(!(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean permissionToLock(DevicePolicyManager mDPM,ComponentName mDeviceAdminSample){
+        if(!mDPM.isAdminActive(mDeviceAdminSample)){
+            askForAdmin(mDeviceAdminSample);
+        }
+        else {
+            return true;
+        }
+        if (mDPM.isAdminActive(mDeviceAdminSample)){
+            return true;
+        }
+        return false;
+    }
+
+
+    //DeviceAdminReceiver declaration - necessary to start service
+    public static class LockReceiver extends DeviceAdminReceiver{
+    }
+
+
 }
